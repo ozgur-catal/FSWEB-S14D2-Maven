f75e60ebf818b2d250123190cefe6bda0ba6f925
ozgur-catal <168709108+ozgur-catal@users.noreply.github.com>
2024-09-25 01:36:23 +0300

    S14G2 gün projesi bitti.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Lamp lamp = new Lamp(LampType.NEON, true, 100);
+        Ceiling ceiling = new Ceiling(3, PaintColor.RED);
+        Bed bed = new Bed("double", 2, 2, 2, 2);
+        Wall northWall = new Wall("North");
+        Wall eastWall = new Wall("East");
+        Wall westWall = new Wall("West");
+        Wall southWall = new Wall("South");
+        Wall[] walls = new Wall[]{northWall, eastWall, southWall, westWall};
+
+
+        Bedroom bedroom = new Bedroom(northWall, eastWall, westWall, southWall, ceiling, lamp,
+                new Carpet(2, 3, PaintColor.WHITE),
+                "bedroom 1", bed, new Wardrobe(1, 2, 100));
+
+        bedroom.createBedroom();
+
+
     }
 }
---
+++ src/main/java/org/example/model/Carpet.java
@@ -0,0 +1,43 @@
+package org.example.model;
+
+import org.example.model.enums.PaintColor;
+
+public class Carpet {
+
+    private PaintColor color;
+
+    private int width;
+
+    private int height;
+
+    public Carpet(int width, int height, PaintColor color) {
+        this.color = color;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void lying() {
+        System.out.println("Carpet is lying on Bedroom floor.");
+    }
+
+    public PaintColor getColor() {
+        return color;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return "Carpet{" +
+                "color=" + color +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
